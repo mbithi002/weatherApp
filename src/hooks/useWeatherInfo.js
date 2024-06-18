e6f1ebdd758320c6ba691646e7d546306ba9cb6f
Lucky Mbithi <luckymbithi002@gmail.com>
2024-06-18 13:34:09 +0300

    remove console.log api key for safety

--- src/hooks/useWeatherInfo.js
+++ src/hooks/useWeatherInfo.js
@@ -8,7 +8,6 @@ const useWeatherInfo = (city) => {
     useEffect(() => {
         const fetchWeatherData = async () => {
             const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
-            console.log(apiKey);
             const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
             try {
