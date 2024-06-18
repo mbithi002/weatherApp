7059d814320f57b4cea0657a183089ec849202b8
Lucky Mbithi <luckymbithi002@gmail.com>
2024-06-18 11:37:48 +0300

    resolve api key failure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env,
+  },
 })
